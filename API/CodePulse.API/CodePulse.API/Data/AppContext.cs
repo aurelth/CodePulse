6dad2e9ae382556889d10d4424876166e9d5a658
Aurel Lossou <aurelth@icloud.com>
2024-02-11 19:35:41 -0300

    DbContext class creation
    DbContext class creation as well as the DbSets (Tables).

---
+++ API/CodePulse.API/CodePulse.API/Data/AppContext.cs
@@ -0,0 +1,12 @@
+﻿using CodePulse.API.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodePulse.API.Data
+{
+    public class AppContext : DbContext
+    {
+        public AppContext(DbContextOptions options) : base(options){ }
+        public DbSet<BlogPost> BlogPosts => Set<BlogPost>();
+        public DbSet<Category> Categories => Set<Category>();
+    }
+}
