ee8c533850bdc4aeee9579c81523a3a741b2a7e8
Aurel Lossou <aurelth@icloud.com>
2024-04-29 01:11:12 -0300

    API call from the angular App, CORS implementation and unsubscribe() implementation

--- UI/codepulse/src/app/features/category/add-category/add-category.component.ts
+++ UI/codepulse/src/app/features/category/add-category/add-category.component.ts
@@ -1,24 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AddCategoryRequest } from '../models/add-category-request.model';
+import { CategoryService } from '../services/category.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-add-category',
   templateUrl: './add-category.component.html',
   styleUrls: ['./add-category.component.css'],
 })
-export class AddCategoryComponent implements OnInit {
-  constructor() {
+export class AddCategoryComponent implements OnDestroy {
+
+  model: AddCategoryRequest;
+  private addCategorySubscription?: Subscription;
+
+
+
+  constructor(private categoryService: CategoryService) {
     this.model = {
       name: '',
       urlHandle: '',
     };
   }
 
-  ngOnInit(): void {}
-
-  model: AddCategoryRequest;
-
   onFormSubmit() {
-    console.log(this.model);
+    this.addCategorySubscription = this.categoryService
+      .addCategory(this.model)
+      .subscribe({
+        next: (response) => {
+          console.log('Successfull');
+        },
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.addCategorySubscription?.unsubscribe();
   }
 }
