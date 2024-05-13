6f85f9b89e7c6b3aa0f5d4d0e5f379e1a1959b46
Aurel Lossou <aurelth@icloud.com>
2024-05-12 23:37:40 -0300

    UI - Create Update Category Service Method and Connect to Edit Category Component

--- UI/codepulse/src/app/features/category/edit-category/edit-category.component.ts
+++ UI/codepulse/src/app/features/category/edit-category/edit-category.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { CategoryService } from '../services/category.service';
 import { Category } from '../models/category.model';
+import { UpdateCategoryRequest } from '../models/update-category-request.model';
 
 @Component({
   selector: 'app-edit-category',
@@ -11,16 +12,14 @@ import { Category } from '../models/category.model';
 })
 export class EditCategoryComponent implements OnInit, OnDestroy {
   id: string | null = null;
-  paramsSubsceiption?: Subscription;
+  paramsSubscription?: Subscription;
+  editCategorySubscription?: Subscription;
   category?: Category;
 
-  constructor(
-    private route: ActivatedRoute,
-    private categoryService: CategoryService
-  ) {}
+  constructor(private route: ActivatedRoute, private categoryService: CategoryService, private router: Router) {}
 
   ngOnInit(): void {
-    this.paramsSubsceiption = this.route.paramMap.subscribe({
+    this.paramsSubscription = this.route.paramMap.subscribe({
       next: (params) => {
         this.id = params.get('id');
 
@@ -36,10 +35,22 @@ export class EditCategoryComponent implements OnInit, OnDestroy {
   }
 
   onFormSubmit(): void {
-    console.log(this.category);
+    const updateCategoryRequest: UpdateCategoryRequest = {
+      name: this.category?.name ?? '',
+      urlHandle: this.category?.urlHandle ?? '',
+    };
+
+    if(this.id){
+      this.editCategorySubscription = this.categoryService.updateCategory(this.id, updateCategoryRequest).subscribe({
+        next: (response) => {
+          this.router.navigateByUrl('admin/categories');
+        }
+      });
+    }
   }
 
   ngOnDestroy(): void {
-    this.paramsSubsceiption?.unsubscribe();
+    this.paramsSubscription?.unsubscribe();
+    this.editCategorySubscription?.unsubscribe();
   }
 }
--- UI/codepulse/src/app/features/category/services/category.service.ts
+++ UI/codepulse/src/app/features/category/services/category.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Category } from '../models/category.model';
 import { environment } from 'src/environments/environment';
+import { UpdateCategoryRequest } from '../models/update-category-request.model';
 
 @Injectable({
   providedIn: 'root',
@@ -21,5 +22,9 @@ export class CategoryService {
 
   getCategoryById(id: string) : Observable<Category> {
     return this.http.get<Category>(`${environment.apiBaseUrl}/api/categories/${id}`);
+  }
+
+  updateCategory(id: string, model: UpdateCategoryRequest): Observable<Category> {
+    return this.http.put<Category>(`${environment.apiBaseUrl}/api/categories/${id}`, model);
   }
 }
