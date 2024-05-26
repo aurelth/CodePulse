81d3cc1e63bd46c4adcd95a6dca64a70a91f5dc4
Aurel Lossou <aurelth@icloud.com>
2024-05-26 20:06:10 -0300

    API - Change Domain Models and Add EF Core Relationships

--- API/CodePulse.API/CodePulse.API/Models/Domain/BlogPost.cs
+++ API/CodePulse.API/CodePulse.API/Models/Domain/BlogPost.cs
@@ -11,5 +11,6 @@
         public DateTime PublishedDate { get; set; }
         public string Author { get; set; } = string.Empty;
         public bool IsVisible { get; set; }
+        public ICollection<Category> Categories { get; set; }
     }
 }
